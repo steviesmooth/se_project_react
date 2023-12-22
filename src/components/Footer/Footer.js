c8d14459c283941e62d4c9500e9d7d2017e55a8d
steviesmooth <naraksteve@yahoo.com>
2023-12-22 14:23:08 -0600

    Update Footer.js
    changed footer tag

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>Developed by TripleTen Students</div>
+      <div>Developed by Steven Narak</div>
       <div>2023</div>
     </footer>
   );
