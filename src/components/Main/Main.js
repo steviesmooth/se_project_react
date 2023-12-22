242f9bae7853fc220ba46d07d90e25609e7e4bd3
steviesmooth <naraksteve@yahoo.com>
2023-12-22 15:49:57 -0600

    rename
    Was dealing with the windows error for when renaming. Figured out how to bypass error

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { defaultClothingItems } from "../../Utils/constants";
+import { defaultClothingItems } from "../../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { useMemo } from "react";
